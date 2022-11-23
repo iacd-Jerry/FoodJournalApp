f9ba7f04cfbd6621cacd93e82e599b141e3d1b74
academicsafe <219777922@tut4life.ac.za>
2022-11-23 20:11:38 +0200

    Skeleton project

---
+++ Food Journal/Entry.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Food Journal
+//
+//  Created by IACD 015 on 2022/11/23.
+//
+
+import UIKit
+
+class EntryController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
